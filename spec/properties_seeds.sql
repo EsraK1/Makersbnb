645f6927bbb91f59da27d161159412602700c39c
EsraK1 <esrakaya165@outlook.com>
2022-09-06 10:14:42 +0100

    changing a typo

--- spec/properties_seeds.sql
+++ spec/properties_seeds.sql
@@ -11,7 +11,6 @@ TRUNCATE TABLE properties RESTART IDENTITY;
 
 -- Our albums table has reference to a foreign key ('artist_id')
 -- because of that we do line 40 and line 49, user_id
-INSERT INTO albums (title, description, price_per_night, user_id) VALUES ('3 bedroom flat', 'Comfortable modern flat!', '£80', '1');
-INSERT INTO albums (title, description, price_per_night, user_id) VALUES ('Semi detached 2 bedroom house', 'Amazing for the weekend', '45', '2');
-INSERT INTO albums (title, description, price_per_night, user_id) VALUES ('Tiny home','Escape to a Romantic and Magical Hobbit Retreat', '£90', '1');
+INSERT INTO properties (title, description, price_per_night, user_id) VALUES ('Semi detached 2 bedroom house', 'Amazing for the weekend', '£45', '2');
+INSERT INTO properties (title, description, price_per_night, user_id) VALUES ('Tiny home','Escape to a Romantic and Magical Hobbit Retreat', '£90', '1');
 INSERT INTO users(username, email_adress, password) VALUES ('Jack', 'JackJones@gmail.com', 'SkyBlue123');
