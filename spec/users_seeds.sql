791bd06756381f19c830894e824a7aeb1f7957cb
shabrez <shabrezhussain@yahooco.uk>
2022-09-06 11:43:50 +0100

    user class

--- spec/users_seeds.sql
+++ spec/users_seeds.sql
@@ -4,8 +4,8 @@
 -- so we can start with a fresh state.
 -- (RESTART IDENTITY resets the primary key)
 
-TRUNCATE TABLE users RESTART IDENTITY; -- replace with your own table name.
-TRUNCATE TABLE properties RESTART IDENTITY;
+TRUNCATE TABLE users, properties RESTART IDENTITY; -- replace with your own table name.
+-- TRUNCATE TABLE properties RESTART IDENTITY;
 
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
