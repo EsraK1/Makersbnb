590dabea02db5eda6f8326c272cd4a254b249c9b
EsraK1 <esrakaya165@outlook.com>
2022-09-06 15:06:32 +0100

    making changes to app.rb

--- app.rb
+++ app.rb
@@ -12,13 +12,4 @@ end
 
 
 # We need to give the database name to the method `connect`.
-DatabaseConnection.connect('Makersbnb')
-
-# Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
-
-# Print out each record from the result set .
-result.each do |record|
-  p record
-end
+DatabaseConnection.connect('makersbnb')
